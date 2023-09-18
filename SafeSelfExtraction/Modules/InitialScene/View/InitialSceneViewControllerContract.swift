738531454227c58ff895bab824e7075b9e4865c6
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-18 16:16:15 +0200

    InitialSceneViewControllerType ch

--- SafeSelfExtraction/Modules/InitialScene/View/InitialSceneViewControllerContract.swift
+++ SafeSelfExtraction/Modules/InitialScene/View/InitialSceneViewControllerContract.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2023 Yaroslav Abaturov. All rights reserved.
 //
 
-protocol InitialSceneViewControllerType {
+protocol InitialSceneViewControllerType: AnyObject {
 	func update(viewModelDataType: InitialSceneViewControllerViewModel.ViewModelDataType)
 }
 
