2803d73f1f23575448e4591c1b35fd8e2e149e21
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-18 16:14:15 +0200

    InitialSceneInteractorServiceType ch

--- SafeSelfExtraction/Modules/InitialScene/Interactor/Service/InitialSceneInteractorService.swift
+++ SafeSelfExtraction/Modules/InitialScene/Interactor/Service/InitialSceneInteractorService.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2023 Yaroslav Abaturov. All rights reserved.
 //
 
-protocol InitialSceneInteractorServiceType{
+protocol InitialSceneInteractorServiceType: AnyObject {
     func performCall(completion: @escaping (Bool) -> ())
 }
 
