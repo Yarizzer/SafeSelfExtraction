3d9e636814fb5b9390897e75094b3515dd1c4cab
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-18 16:15:54 +0200

    InitialScenePresentable ch

--- SafeSelfExtraction/Modules/InitialScene/Presenter/InitialScenePresenterContract.swift
+++ SafeSelfExtraction/Modules/InitialScene/Presenter/InitialScenePresenterContract.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2023 Yaroslav Abaturov. All rights reserved.
 //
 
-protocol InitialScenePresentable {
+protocol InitialScenePresentable: AnyObject {
 	func response(responseType: InitialScenePresenterResponse.InitialSceneResponseType)
 }
 
